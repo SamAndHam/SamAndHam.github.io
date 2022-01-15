78b3ab275e7ebcbed6954ad5a56954b0dcbdceb0
fpoloz@gmail.com <fpoloz@gmail.com>
2022-01-15 18:44:25 +0800

    Update javascript for cleaner loading

--- test.js
+++ test.js
@@ -6,22 +6,81 @@ var Base_URL = window.location.href;
 var historyList = [];  //To maintain history
 
 var JSON_STATC_DATA;   // List of files JSON 
+var JSON_PAGE_METADATA = [];  // List of pages sorted
+var TOPIC_SET = new Set();    // Set of all the topics
+var TOPIC_SELECTED = "";      // Topic filter. if blank no filter.
+
+
 var JSON_HEADER_KEYS;
 var JSON_DATE_KEYS = new Set(); //Set of dates, Biggest to smallest
 
-
+// create struct to store topics.
+// topics -> date
+//        -> topic
+//        -> title
+//        -> tags
+//        -> title
+//        -> synopsis
+//        -> Link to actual data
+//
+function populateTopics(node, articleDate) 
+{
+  this.articleDate  = articleDate;
+  this.Topic        = node.Topic;
+  this.Title        = node.Title;
+  this.Tags         = node.Tags;
+  this.Synopsis     = node.Synopsis;
+  this.Path         = node.Path;
+}
+//TODO insert DATE into json itself. instead of another nest
+ 
 function initList() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
       JSON_STATC_DATA = JSON.parse(xhr.responseText);
+      //Struct
+      //  Path
+      //  FileName
+      //  Topic
+      //  Date
+      //  Tags
+      //  Synopsis
       var keys = Object.keys(JSON_STATC_DATA);
+
+      //extract all page metadata
+      for ( var x  in JSON_STATC_DATA ) {
+        for ( var y in JSON_STATC_DATA[x] ) {
+          for (var  i = 0; i <  JSON_STATC_DATA[x][y].length; i++) {
+            JSON_PAGE_METADATA.push(JSON_STATC_DATA[x][y][i]);
+            TOPIC_SET.add( JSON_STATC_DATA[x][y][i].Topic);
+          }
+        }
+      }
+
+      JSON_PAGE_METADATA.sort(function(a,b){
+        var dateA  = new Date(a.Date);
+        var dateB  = new Date(b.Date);
+        return dateB - dateA;
+      });
+     
+      //DEBUG 
+      for ( var x in JSON_PAGE_METADATA ) {
+        console.log(JSON_PAGE_METADATA[x]);
+      }
+
+      TOPIC_SET.forEach((value) => {console.log(value)});
+      //DEBUG
+
+      //========
       //retrieve all date keys, sort and store
       var tmpDate = []
       for ( var i = 0 ; i < keys.length; i++) {
         var dates = Object.keys(JSON_STATC_DATA[keys[i]]);
+
         for (var z =0 ; z < dates.length; z ++) {
           tmpDate.push(new Date(dates[z]));
+          //console.log(JSON_STATC_DATA[keys[i]][dates[z]]);
         }
       } 
       tmpDate.sort(function(a,b) {
@@ -124,6 +183,23 @@ function parseElementContent(num, SelectedState, SelectedDate) {
 //Header Div -> Dates
 function loadHeader() { 
   JSON_HEADER_KEYS = Object.keys(JSON_STATC_DATA);
+
+  document.getElementById("topics").innerHTML = "";
+ 
+  TOPIC_SET.forEach( value => {
+    
+    var topicDiv = document.createElement("DIV");
+    topicDiv.className = "button";
+    topicDiv.innerHTML = value;
+    topicDiv.onclick = function() { 
+      TOPIC_SELECTED = this.innerHTML; 
+      //console.log(this.innerHTML);
+      
+      //reload page here
+    };
+    document.getElementById("topics").appendChild(topicDiv);
+    
+  }); 
 
 }
 
